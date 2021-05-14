498af67edda7a645f4e880feab7b48ee6fefe992
Jean-Francois Teddy <jfrancoisteddy@gmail.com>
2021-05-14 18:13:10 +0200

    fix: add timestamp inside invitation

--- src/models/Invitation.model.ts
+++ src/models/Invitation.model.ts
@@ -13,6 +13,8 @@ const invitationSchema = new Schema({
     
     team_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Team' },
     user_id: {  type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+},{
+    timestamps: true 
 });
 
 const Invitation = mongoose.model<IInvitation>('Invitation', invitationSchema);
