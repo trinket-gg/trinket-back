b5aa01de2c81298c2afccf808758e2a3d0fe3131
HHK <hhk.contact@protonmail.com>
2021-05-31 14:20:12 +0200

    feat: add socket.io package

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import express, { Express } from "express";
-import bodyParser from "body-parser";
-
-import { config } from "dotenv";
-config();
-
 import { buildRoutes } from "./routes";
+import { config } from "dotenv";
+import * as socketio from 'socket.io';
+config();
 
 const app: Express = express();
 
-const path = require('path')
 const mongoose = require('mongoose')
 
 try {
@@ -22,6 +19,12 @@ app.use(express.json())
 buildRoutes(app)
 
 const port = 4000;
-app.listen(port, '0.0.0.0', function() {
+const server = app.listen(port, '0.0.0.0', function() {
     console.log(`Listening on ${port}...`);
 });
+
+const io = require('socket.io')(server);
+
+io.on('connection', (socket: socketio.Socket) => {
+    console.log('connection');
+})
